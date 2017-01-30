caf383282fb567333c76a4344f5b17a6bafe2d16
Dmitry Petrochenkov <demon-st2002@yandex.ru>
2017-01-30 21:30:50 +0300

    tic-tac-toe: package.json - make autostart test; tic-tac-toe.js make constructor, getCurrentPlayerSymbol()

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,22 +1,38 @@
 class TicTacToe {
     constructor() {
-
+        this.player1 = null;
+        this.player2 = null;
+        this.currentPlayer = null;
+        this.rowIndex = null;
+        this.columnIndex = null;
+        this.counter = 1;
     }
 
     getCurrentPlayerSymbol() {
-
+        if(this.currentPlayer = this.player1){
+            return 'x';
+        } else if (this.currentPlayer === this.player2){
+            return 'o';
+        };
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if(this.rowIndex === this.columnIndex === 0){
+            this.currentPlayer = player1;
+        }
 
     }
 
     isFinished() {
-
+        if (counter === 9){
+            return true;
+        } else return false;
     }
 
     getWinner() {
+        if(isFinished()){
 
+        } else return null
     }
 
     noMoreTurns() {
@@ -27,7 +43,7 @@ class TicTacToe {
 
     }
 
-    getFieldValue(rowIndex, colIndex) {
+    getFieldValue(rowIndex, columnIndex) {
 
     }
 }
